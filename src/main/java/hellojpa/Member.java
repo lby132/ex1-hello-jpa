876f2e07ffe26ba5a86ec62c1b0ce95154c241f9
lby132 <quddbscjswow@naver.com>
2022-10-14 23:51:12 +0900

    JPA연습

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity	// JPA가 이 객체를 사용해야겠다는걸 인식함
+public class Member {
+
+    @Id	// JPA에게 primary key를 알려줘야함
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
